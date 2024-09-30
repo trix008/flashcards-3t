8e8e45c0ef13c4a55c26a022f5cac7636394bfee
trix008 <161336767+trix008@users.noreply.github.com>
2024-09-30 10:40:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Progamação',
+    'O que é JavaScript',
+    'É uma linguagem de progamação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Progamação',
+    'O que é uma função',
+    'Uma funçao é um bloco de código que faz alguma tarefa'
+)
+
+criaCartao(
+    'Progamação',
+    'O que é Phyton',
+    'É uma linguagem de progamação'
+)
